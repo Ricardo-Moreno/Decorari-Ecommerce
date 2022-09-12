5a3d26d1416265089a5eb1d4f34fb33aba818178
Ricardo Moreno <ricardo.yvl@gmail.com>
2022-09-12 00:38:09 -0300

    desafios 2 y 3

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import decorariLogo from '../components/assets/decorariLogo.png';
+
+
+function CartWidget() {
+  return (
+    <> 
+    <img className="img-fluid" width='200' src={ decorariLogo } alt='Logo Decorari'/>
+    </>
+  )
+}
+
+export default CartWidget
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from './CartWidget';
+import './NavBar.scss';
+
+
+
+function NavBar() {
+  return (
+    <>
+       <Navbar bg="danger" variant="blue">
+        <Container>
+          <Navbar.Brand href="#home">
+            <CartWidget/>
+             </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Contactos</Nav.Link>
+            <Nav.Link href="#pricing">Categorias</Nav.Link>
+            <NavDropdown title="Categorias" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action1">Arreglos</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action2">Decoraciones</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action3">Vnilos</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+      </>
+  )
+}
+
+export default NavBar
