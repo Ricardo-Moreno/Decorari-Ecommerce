cdcbe9bd615468868fab940acbe926f781938824
Ricardo Moreno <ricardo.yvl@gmail.com>
2022-10-18 17:03:44 -0300

    FireBase 4

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,21 +2,24 @@ import React, { useState, useEffect } from 'react';
 import ItemDetail from '../ItemDetail/ItemDetail';
 import { getFirestore, doc, getDoc} from 'firebase/firestore'
 import { RaceBy } from '@uiball/loaders';
+import { useParams } from 'react-router-dom';
 
 function ItemDetailContainer() {
   const [itemLis, setitemLis] = useState ({})
   const [loading, setLoading] = useState(false)
-
+  const {detalleId} = useParams()
 
     useEffect(()=>{
       const db  = getFirestore()
-      const dbQuery = doc(db, 'items', '3HQHVjWTVuN1uIVsW9HK')
+      const dbQuery = doc(db, 'items', detalleId)
       getDoc(dbQuery)
       .then(resp => setitemLis({id: resp.id, ...resp.data()}))
       .catch((err)=> console.log(err))
       .finally (()=>setLoading(true))
-  }, [])
+  }, [detalleId])
 
+
+  console.log(detalleId)
 
   return (
     <>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,27 +2,37 @@ import ItemList from '../ItemList/ItemList'
 import { useState, useEffect } from 'react';
 import { RaceBy } from '@uiball/loaders'
 import './ItemListContainer.css'
-import { getFirestore, collection, getDocs} from 'firebase/firestore'
+import { getFirestore, collection, getDocs, query, where} from 'firebase/firestore'
+import { useParams } from 'react-router-dom';
 
 
 function ItemListContainer() {
 
 const [itemList, setitemList] = useState ([])
 const [loading, setLoading] = useState(false)
-
+const {id} = useParams()
 
 useEffect(()=>{
     const db  = getFirestore()
 
+    if (id){
+
+    const queryCollection = collection(db, 'items')
+    const queryCollectionFilter = query(queryCollection, where('category', '==', id))
+    getDocs(queryCollectionFilter)
+    .then(resp => setitemList(resp.docs.map(item => ({id: item.id, ...item.data()} ))))
+    .catch((err)=> console.log(err))
+    .finally (()=>setLoading(true))
+   } else {
     const queryCollection = collection(db, 'items')
     getDocs(queryCollection)
     .then(resp => setitemList(resp.docs.map(item => ({id: item.id, ...item.data()} ))))
     .catch((err)=> console.log(err))
     .finally (()=>setLoading(true))
+ 
+}},[id])
 
-
-},[])
-
+console.log(id)
 
 console.log(itemList)
 
